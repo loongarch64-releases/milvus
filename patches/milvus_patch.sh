30550bb05e456eddd891a6daa4f48c13e0ea618d
Chris-wh233 <128221293+Chris-wh233@users.noreply.github.com>
2026-02-06 17:00:22 +0800

    adjust milvus_patch.sh
    Removed commented-out code for allowing multiple definitions and updated the path for libmilvus_core.so.

--- patches/milvus_patch.sh
+++ patches/milvus_patch.sh
@@ -15,10 +15,6 @@ sed -i "s/set(CARGO_CMD cargo +1.89 build/set(CARGO_CMD cargo build/" "$src/inte
 # 修改 conan install，通过 profile 指定 loongarch 环境
 sed -i "s/-s build_type=\${BUILD_TYPE} -s compiler.version=\${GCC_VERSION} -s compiler.libcxx=libstdc++11/-pr:b=loongarch -pr:h=loongarch/" "$src/scripts/3rdparty_build.sh"
 
-# 允许重复定义
-#sed -i '/\${CMAKE_EXTRA_ARGS}/a\
-#-DCMAKE_SHARED_LINKER_FLAGS="-Wl,--allow-multiple-definition" \\' "$src/scripts/core_build.sh"
-
 # 纠正 libmilvus_core.so 路径
 sed -i "s/lib\/libmilvus_core.so/lib64\/libmilvus_core.so/" "$src/scripts/setenv.sh"
 
