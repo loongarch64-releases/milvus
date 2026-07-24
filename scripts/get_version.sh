07ac1d273e5d13c97130a473cef72893f2a12946
wuhao <wuhao01@loongson.cn>
2026-07-24 10:00:12 +0800

    Use tag as the basis for whether to trigger ci

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -4,5 +4,15 @@ set -eou pipefail
 UPSTREAM_OWNER=milvus-io
 UPSTREAM_REPO=milvus
 
-curl -s https://api.github.com/repos/"$UPSTREAM_OWNER"/"$UPSTREAM_REPO"/releases/latest \
-     | jq -r ".tag_name"
+# 最新的 release
+#curl -s https://api.github.com/repos/"$UPSTREAM_OWNER"/"$UPSTREAM_REPO"/releases/latest \
+#     | jq -r ".tag_name"
+
+# 最新的 tag
+git ls-remote --tags https://github.com/${UPSTREAM_OWNER}/${UPSTREAM_REPO}.git \
+    | cut -d'/' -f3- \
+    | cut -d'^' -f1 \
+    | grep -E '^v[0-9]{1,2}.[0-9]+.[0-9]+$' \
+    | sort -V \
+    | uniq \
+    | tail -1
